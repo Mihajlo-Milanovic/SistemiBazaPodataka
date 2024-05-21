4d8e28812a8524e510e9af3534306063268d27e0
Mihajlo <milanovic.mihajlo@elfak.rs>
2024-05-21 10:10:37 +0200

    Ispravljen copy konstruktor za PolicajacView

--- Drugi_deo/PolicijskaUprava/PolicijskaUprava/DTOs/PolicajacView.cs
+++ Drugi_deo/PolicijskaUprava/PolicijskaUprava/DTOs/PolicajacView.cs
@@ -36,46 +36,65 @@ namespace PolicijskaUprava.DTOs {
 			ZamenikStaniceId = -1;
 			ZamenikStaniceNaziv = null;
 		}
-		public PolicajacView(int id, string ime, string imeRoditelja, string prezime, DateTime datumRodjenja,
-							string jmbg, string adresa, DateTime datumPrijemaUSluzbu, PolicijskaStanicaView stanica,
-							PolicijskaStanicaView sefuje, PolicijskaStanicaView zamenik, string tip) {
-			Id = id;
-			Ime = ime;
-			ImeRoditelja = imeRoditelja;
-			Prezime = prezime;
-			DatumRodjenja = datumRodjenja;
-			JMBG = jmbg;
-			Adresa = adresa;
-			DatumPrijemaUSluzbu = datumPrijemaUSluzbu;
-			StanicaId = stanica.Id;
-			StanicaNaziv = stanica.Naziv;
-			SefujeStanicomId = sefuje.Id;
-			SefujeStanicomNaziv = sefuje.Naziv;
-			ZamenikStaniceId = zamenik.Id;
-			ZamenikStaniceNaziv = zamenik.Naziv;
-			Tip = tip;
-		}
+		//public PolicajacView(int id, string ime, string imeRoditelja, string prezime, DateTime datumRodjenja,
+		//					string jmbg, string adresa, DateTime datumPrijemaUSluzbu, PolicijskaStanicaView stanica,
+		//					PolicijskaStanicaView sefuje, PolicijskaStanicaView zamenik, string tip) {
+		//	Id = id;
+		//	Ime = ime;
+		//	ImeRoditelja = imeRoditelja;
+		//	Prezime = prezime;
+		//	DatumRodjenja = datumRodjenja;
+		//	JMBG = jmbg;
+		//	Adresa = adresa;
+		//	DatumPrijemaUSluzbu = datumPrijemaUSluzbu;
+		//	StanicaId = stanica.Id;
+		//	StanicaNaziv = stanica.Naziv;
+		//	SefujeStanicomId = sefuje.Id;
+		//	SefujeStanicomNaziv = sefuje.Naziv;
+		//	ZamenikStaniceId = zamenik.Id;
+		//	ZamenikStaniceNaziv = zamenik.Naziv;
+		//	Tip = tip;
+		//}
 
-		public PolicajacView(Policajac p){ 
-		
+		public PolicajacView(Policajac p) {
+
 			Id = p.Id;
 			Ime = p.Ime;
 			ImeRoditelja = p.ImeRoditelja;
 			Prezime = p.Prezime;
 			DatumRodjenja = p.DatumRodjenja;
-			JMBG= p.JMBG;
+			JMBG = p.JMBG;
 			Adresa = p.Adresa;
 			DatumPrijemaUSluzbu = p.DatumPrijemaUSluzbu;
 			Tip = p.Tip;
 
-			StanicaId = p.Stanica.Id;
-			StanicaNaziv = p.Stanica.Naziv;
+			if (p.Stanica != null) {
+				StanicaId = p.Stanica.Id;
+				StanicaNaziv = p.Stanica.Naziv;
+			}
+			else {
+				StanicaId = -1;
+				StanicaNaziv = null;
+			}
 
-			SefujeStanicomId = p.SefujeStanicom.Id;
-			SefujeStanicomNaziv = p.SefujeStanicom.Naziv;
-			
-			ZamenikStaniceId = p.ZamenikStanice.Id;
-			ZamenikStaniceNaziv = p.ZamenikStanice.Naziv;
+			if (p.SefujeStanicom != null) {
+				SefujeStanicomId = p.SefujeStanicom.Id;
+				SefujeStanicomNaziv = p.SefujeStanicom.Naziv;
+			}
+			else {
+				SefujeStanicomId = -1;
+				SefujeStanicomNaziv = null;
+			}
+
+			if (p.ZamenikStanice != null) {
+				ZamenikStaniceId = p.ZamenikStanice.Id;
+
+				ZamenikStaniceNaziv = p.ZamenikStanice.Naziv;
+			}
+			else {
+				ZamenikStaniceId = -1;
+				ZamenikStaniceNaziv = null;
+			}
 		}
 	}
 }
